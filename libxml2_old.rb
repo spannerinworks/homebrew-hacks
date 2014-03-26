5d1c52ca504c1808e9289fb7c574be3db0abcae4
Ben Wong <ben.wong@unboxedconsulting.com>
2014-03-26 16:35:35 +0000

    libxml2 for channel 5

---
+++ libxml2_old.rb
@@ -0,0 +1,59 @@
+require 'formula'
+
+class Libxml2Old < Formula
+  homepage 'http://xmlsoft.org'
+
+  stable do
+    url 'ftp://xmlsoft.org/libxml2/old/libxml2-2.6.26.tar.gz'
+    mirror 'http://xmlsoft.org/sources/old/libxml2-2.6.26.tar.gz'
+    sha256 '6fac5d751e078780f0709563bc6db37380f159852e13281f42397ccbcac415ed'
+  end
+
+  bottle do
+    sha1 "1971dcb3cfb9a68555ee6eaff7c83d0dd8109442" => :mavericks
+    sha1 "558653754996428880b14123dac08baa639f97c3" => :mountain_lion
+    sha1 "93b5c4b98475f71cbf1915b2293df14d7621b262" => :lion
+  end
+
+  head do
+    url 'https://git.gnome.org/browse/libxml2', :using => :git
+
+    depends_on :autoconf
+    depends_on :automake
+    depends_on :libtool
+  end
+
+  depends_on :python => :recommended
+
+  keg_only :provided_by_osx
+
+  option :universal
+
+  fails_with :llvm do
+    build 2326
+    cause "Undefined symbols when linking"
+  end
+
+  def install
+    ENV.universal_binary if build.universal?
+    if build.head?
+      inreplace 'autogen.sh', 'libtoolize', 'glibtoolize'
+      system './autogen.sh'
+    end
+
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--without-python"
+    system "make"
+    ENV.deparallelize
+    system "make install"
+
+    if build.with? 'python'
+      cd 'python' do
+        # We need to insert our include dir first
+        inreplace 'setup.py', 'includes_dir = [', "includes_dir = ['#{include}', '#{MacOS.sdk_path}/usr/include',"
+        system "python", 'setup.py', "install", "--prefix=#{prefix}"
+      end
+    end
+  end
+end
